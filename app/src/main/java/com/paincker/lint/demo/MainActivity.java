086eacc24bf7243e5fda19a50cabeea5de0ee830
zhengenguo <zhengenguo@gmail.com>
2018-12-24 16:37:21 +0800

    pre-commit-1

---
+++ app/src/main/java/com/paincker/lint/demo/MainActivity.java
@@ -0,0 +1,72 @@
+package com.paincker.lint.demo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "toast", Toast.LENGTH_SHORT);
+
+        callNewApi();
+        Log.d("tag", "msg");
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+
+
+            }
+        }).run();
+
+
+
+        Toast.makeText(this, "version_code= " + getVersionCode(this), Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "version_name= " + getVersionName(this), Toast.LENGTH_LONG).show();
+    }
+
+    public String getVersionName(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        PackageInfo packageInfo;
+        String versionCode = "";
+        try {
+            packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+            versionCode = packageInfo.versionName + "";
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return versionCode;
+    }
+
+    public String getVersionCode(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        PackageInfo packageInfo;
+        String versionCode = "";
+        try {
+            packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+            versionCode = packageInfo.versionCode + "";
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return versionCode;
+    }
+
+    private void callNewApi() {
+        new View(this).setOnScrollChangeListener(new View.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+
+            }
+        });
+    }
+}
